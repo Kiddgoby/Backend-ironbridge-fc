cd02bd20513b9028a396175e5e74c5e5cc9ce236
Arnau Gausachs <arnau.gausachs.gancedo@gmail.com>
2026-03-09 18:20:35 +0100

    CRUD de milestone y funcionando

---
+++ src/Controller/MilestoneController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Milestone;
+use App\Repository\MilestoneRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[Route('/api/milestones')]
+final class MilestoneController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private MilestoneRepository $milestoneRepository,
+        private SerializerInterface $serializer
+    ) {}
+
+
+    #[Route('', name: 'api_milestone_index', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        $milestones = $this->milestoneRepository->findAll();
+        $json = $this->serializer->serialize($milestones, 'json', ['groups' => 'milestone:read']);
+
+        return new JsonResponse($json, Response::HTTP_OK, [], true);
+    }
+
+
+    #[Route('/{id}', name: 'api_milestone_show', methods: ['GET'])]
+    public function show(int $id): JsonResponse
+    {
+        $milestone = $this->milestoneRepository->find($id);
+
+        if (!$milestone) {
+            return new JsonResponse(['error' => 'Milestone not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $json = $this->serializer->serialize($milestone, 'json', ['groups' => 'milestone:read']);
+
+        return new JsonResponse($json, Response::HTTP_OK, [], true);
+    }
+
+
+    #[Route('', name: 'api_milestone_create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        try {
+            $milestone = $this->serializer->deserialize($request->getContent(), Milestone::class, 'json');
+            
+            $this->entityManager->persist($milestone);
+            $this->entityManager->flush();
+
+            $json = $this->serializer->serialize($milestone, 'json', ['groups' => 'milestone:read']);
+
+            return new JsonResponse($json, Response::HTTP_CREATED, [], true);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+
+    #[Route('/{id}', name: 'api_milestone_update', methods: ['PUT', 'PATCH'])]
+    public function update(int $id, Request $request): JsonResponse
+    {
+        $milestone = $this->milestoneRepository->find($id);
+
+        if (!$milestone) {
+            return new JsonResponse(['error' => 'Milestone not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        try {
+            $this->serializer->deserialize(
+                $request->getContent(),
+                Milestone::class,
+                'json',
+                ['object_to_populate' => $milestone]
+            );
+
+            $this->entityManager->flush();
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    
+    #[Route('/{id}', name: 'api_milestone_delete', methods: ['DELETE'])]
+    public function delete(int $id): JsonResponse
+    {
+        $milestone = $this->milestoneRepository->find($id);
+
+        if (!$milestone) {
+            return new JsonResponse(['error' => 'Milestone not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $this->entityManager->remove($milestone);
+        $this->entityManager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+}
